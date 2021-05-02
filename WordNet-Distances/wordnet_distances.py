4e3898fd63b843aee37890b2bfd3199a196e21ef
luisegarduno <luis.e.garduno@gmail.com>
2021-05-02 00:46:38 -0500

    - Path & Wup Similarity done

--- WordNet-Distances/wordnet_distances.py
+++ WordNet-Distances/wordnet_distances.py
@@ -1,12 +1,23 @@
 from nltk.corpus import wordnet as wn
+from nltk.corpus import genesis
+from nltk.corpus import wordnet_ic
 
+# -------------------------------------------------------------------------
+def builder(w1, w2, pos1 = None, pos2 = None):
+    a,b = [],[]
+
+    if pos1 == None and pos2 == None: a,b = wn.synsets(w1), wn.synsets(w2)
+    if pos1 == None and pos2 != None: a,b = wn.synsets(w1), wn.synsets(w2, pos=pos2)
+    if pos1 != None and pos2 == None: a,b = wn.synsets(w1, pos=pos1), wn.synsets(w2)
+    if pos1 != None and pos2 != None: a,b = wn.synsets(w1, pos=pos1), wn.synsets(w2, pos=pos2)
+
+    return a,b
 
 # -------------------------------------------------------------------------
 def word_path_similarity(w1, w2, pos1=None, pos2=None, option=None):
     print("word_path_similarity ============================================")
 
-    a = wn.synsets(w1)
-    b = wn.synsets(w2)
+    a,b = builder(w1,w2,pos1,pos2)
 
     # Option : First (None)
     if option == None or option == 'first':
@@ -25,10 +36,14 @@ def word_path_similarity(w1, w2, pos1=None, pos2=None, option=None):
 
     # Option : Minimum
     if option == "min":
-        print("hi")
+        c = []
+        for a_i in range(len(a)):
+            for b_i in range(len(b)):
+                p = wn.path_similarity(a[a_i],b[b_i])
+                if p != None: c.append(p)
+        print("\tPath similarity =", min(c))
 
     print()
-
 
 
 
@@ -36,8 +51,7 @@ def word_path_similarity(w1, w2, pos1=None, pos2=None, option=None):
 def word_lcs_similarity(w1, w2, pos1=None, pos2=None, option=None):
     print("word_lcs_similarity ============================================")
 
-    a = wn.synsets(w1)
-    b = wn.synsets(w2)
+    a,b = builder(w1,w2,pos1,pos2)
 
     # Option : First (None)
     if option == None or option == 'first':
@@ -60,10 +74,15 @@ def word_lcs_similarity(w1, w2, pos1=None, pos2=None, option=None):
 
     # Option : Minimum
     if option == "min":
-        print("hi")
+        c = []
+        for a_i in range(len(a)):
+            for b_i in range(len(b)):
+                p = wn.path_similarity(a[a_i],b[b_i])
+                if p != None: c.append(p)
+                i += 1.0
+        print("\tPath similarity =", min(c))
 
     print()
-
 
 
 
@@ -71,8 +90,7 @@ def word_lcs_similarity(w1, w2, pos1=None, pos2=None, option=None):
 def word_wup_similarity(w1, w2, pos1=None, pos2=None, option=None):
     print("word_wup_similarity ============================================")
 
-    a = wn.synsets(w1)
-    b = wn.synsets(w2)
+    a,b = builder(w1,w2,pos1,pos2)
 
     # Option : First (None)
     if option == None or option == 'first':
@@ -91,12 +109,14 @@ def word_wup_similarity(w1, w2, pos1=None, pos2=None, option=None):
 
     # Option : Minimum
     if option == "min":
-        print("hi")
-
-
+        c = []
+        for a_i in range(len(a)):
+            for b_i in range(len(b)):
+                p = wn.path_similarity(a[a_i],b[b_i])
+                if p != None: c.append(p)
+        print("\tPath similarity =", min(c))
 
     print()
-
 
 
 # -------------------------------------------------------------------------
@@ -107,22 +127,109 @@ def word_res_similarity(ic, w1, w2, pos1=None, pos2=None, option=None):
 
 
 
-
 #################################################################################
 ############################## TESTING ##########################################
 #################################################################################
 w1 = "dog"
 w2 = "cat"
 
+testing = "wup"
 
-one = word_path_similarity(w1, w2, option="avg")
-# print(one)
+# Path Similarity : DONE ########################################################
 
-two = word_lcs_similarity(w1, w2, option="avg")
-# print(two)
+""" Word Path Similarity : first """
+if testing == "first_ps" or testing == "ps" or testing == "all":
+    word_path_similarity(w1, w2, option="first")
+    word_path_similarity(w1, w2, pos1='n', option="first")
+    word_path_similarity(w1, w2, pos2='n', option="first")
+    word_path_similarity(w1, w2, pos1='n', pos2='n', option="first")
 
-three = word_wup_similarity(w1, w2, option="avg")
-# print(three)
+""" Word Path Similarity : avg """
+if testing == "avg_ps" or testing == "ps" or testing == "all":
+    word_path_similarity(w1, w2, option="avg")
+    word_path_similarity(w1, w2, pos1='n', option="avg")
+    word_path_similarity(w1, w2, pos2='n', option="avg")
+    word_path_similarity(w1, w2, pos1='n', pos2='n', option="avg")
+
+""" Word Path Similarity : min """
+if testing == "min_ps" or testing == "ps" or testing == "all":
+    word_path_similarity(w1, w2, option="min")
+    word_path_similarity(w1, w2, pos1='n', option="min")
+    word_path_similarity(w1, w2, pos2='n', option="min")
+    word_path_similarity(w1, w2, pos1='n', pos2='n', option="min")
+
+# LCS Similarity : !Done ########################################################
+
+""" Word Lcs Similarity : first """
+if testing == "first_lcs" or testing == "lcs" or testing == "all":
+    word_lcs_similarity(w1, w2, option="first")
+    word_lcs_similarity(w1, w2, pos1='n', option="first")
+    word_lcs_similarity(w1, w2, pos2='n', option="first")
+    word_lcs_similarity(w1, w2, pos1='n', pos2='n', option="first")
+
+""" Word Lcs Similarity : avg """
+if testing == "avg_lcs" or testing == "lcs" or testing == "all":
+    word_lcs_similarity(w1, w2, option="avg")
+    word_lcs_similarity(w1, w2, pos1='n', option="avg")
+    word_lcs_similarity(w1, w2, pos2='n', option="avg")
+    word_lcs_similarity(w1, w2, pos1='n', pos2='n', option="avg")
+
+""" Word Lcs Similarity : min """
+if testing == "min_lcs" or testing == "lcs" or testing == "all":
+    word_lcs_similarity(w1, w2, option="min")
+    word_lcs_similarity(w1, w2, pos1='n', option="min")
+    word_lcs_similarity(w1, w2, pos2='n', option="min")
+    word_lcs_similarity(w1, w2, pos1='n', pos2='n', option="min")
+
+# Wup Similarity : Done #########################################################
+
+""" Word Wup Similarity : first """
+if testing == "first_wup" or testing == "wup" or testing == "all":
+    word_wup_similarity(w1, w2, option="first")
+    word_wup_similarity(w1, w2, pos1='n', option="first")
+    word_wup_similarity(w1, w2, pos2='n', option="first")
+    word_wup_similarity(w1, w2, pos1='n', pos2='n', option="first")
+
+""" Word Wup Similarity : avg """
+if testing == "avg_wup" or testing == "wup" or testing == "all":
+    word_wup_similarity(w1, w2, option="avg")
+    word_wup_similarity(w1, w2, pos1='n', option="avg")
+    word_wup_similarity(w1, w2, pos2='n', option="avg")
+    word_wup_similarity(w1, w2, pos1='n', pos2='n', option="avg")
+
+""" Word Wup Similarity : min """
+if testing == "min_wup" or testing == "wup" or testing == "all":
+    word_wup_similarity(w1, w2, option="min")
+    word_wup_similarity(w1, w2, pos1='n', option="min")
+    word_wup_similarity(w1, w2, pos2='n', option="min")
+    word_wup_similarity(w1, w2, pos1='n', pos2='n', option="min")
+
+# Res Similarity : !Done ########################################################
+
+""" Word Res Similarity : first """
+if testing == "first_res" or testing == "res" or testing == "all":
+    ic = wn.ic(genesis, False, 0.0)
+    word_res_similarity(ic, w1, w2, option="first")
+    word_res_similarity(ic, w1, w2, pos1='n', option="first")
+    word_res_similarity(ic, w1, w2, pos2='n', option="first")
+    word_res_similarity(ic, w1, w2, pos1='n', pos2='n', option="first")
+
+""" Word Res Similarity : avg """
+if testing == "avg_res" or testing == "res" or testing == "all":
+    ic = wn.ic(genesis, False, 0.0)
+    word_res_similarity(ic, w1, w2, option="avg")
+    word_res_similarity(ic, w1, w2, pos1='n', option="avg")
+    word_res_similarity(ic, w1, w2, pos2='n', option="avg")
+    word_res_similarity(ic, w1, w2, pos1='n', pos2='n', option="avg")
+
+""" Word Res Similarity : min """
+if testing == "min_res" or testing == "res" or testing == "all":
+    ic = wn.ic(genesis, False, 0.0)
+    word_res_similarity(ic, w1, w2, option="min")
+    word_res_similarity(ic, w1, w2, pos1='n', option="min")
+    word_res_similarity(ic, w1, w2, pos2='n', option="min")
+    word_res_similarity(ic, w1, w2, pos1='n', pos2='n', option="min")
+
 
 # four = word_res_similarity(ic, w1, w2)
 # print(four)
