dd806ef6fb3594a02d31ec528585c4ee0736d101
luisegarduno <luis.e.garduno@gmail.com>
2022-03-30 02:27:52 -0500

    - Minor updates

--- BigramModel_AutoCorrect/word-bigram.py
+++ BigramModel_AutoCorrect/word-bigram.py
@@ -21,22 +21,29 @@ def bgs(w):
         return True
     return False
 
+avg = 0
 # Check to see if program has been ran before
-if os.path.isfile('test/top-1k-words.txt'):
+if os.path.isfile('test/top-100-words.txt'):
     dex = []
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    with open('test/top-1k-words.txt', 'r+') as file:
+    with open('test/top-100-words.txt', 'r+') as file:
         word = "hi"
-        while str.lower(word) != "yourself":
+        while str.lower(word) != "your":
             word = file.readline()
             word = word.replace('\n', '')
+            avg += len(word)
+            #if len(word) > 7:
+                #print(word)
             dex.append(word)
     file.close()
+
+avg /= 100
+print("Average length: ", avg)
 
 winners = []
 for i in range(0,len(dex)):
     if bgs(dex[i]):
         winners.append(dex[i]);
 
-print("# of words containing bigram 'TH' & 'AT': ", len(winners))
+#print("# of words containing bigram 'TH' & 'AT': ", len(winners))
