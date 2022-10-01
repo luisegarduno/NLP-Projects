3cf9ce740d440988dfb98224e8af2b35456cf1e2
luisegarduno <luis.e.garduno@gmail.com>
2022-10-01 08:49:32 -0500

    - Working on adding multiple documents now

--- tf-idf.py
+++ tf-idf.py
@@ -6,19 +6,19 @@ st.title('TF-IDF Tool')
 if 'counter' not in st.session_state:
     st.session_state['counter'] = 0
 
-increment = st.button('Increment')
-if increment:
+dictionary = []
+
+add_doc = st.button('Add document')
+if add_doc:
     st.session_state['counter'] += 1
     st.write(st.session_state['counter'])
-
+    dictionary.append(st.text_input('Document #' + str(st.session_state['counter'])))
 
 st.markdown('-------------------')
 
 st.markdown('__Raw Frequency (TF)__: $f_{ij}$')
 st.markdown('__Inverse Frequency (IDF)__: $idf_{i} = \log_{2}(\dfrac{N}{n_{i}})$')
-
 st.markdown('__TF-IDF__: $w_{ij} = tf_{ij} * idf_{i} = (1 + \log_{2}(f_{ij})) * \log_{2}(\dfrac{N}{n_{i}})$')
-
 st.markdown('__Cosine Similarity__: $\sum{k} (d_{kj} * q_{ki})$')
 
 st.markdown('-------------------')
